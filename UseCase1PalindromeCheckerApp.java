588057e7f01c6eb469b3d6faad058edb0c3ea82f
Danga69-ngl <khushhaldanga1@gmail.com>
2026-03-16 15:06:32 +0530

    added usecase3

--- UseCase1PalindromeCheckerApp.java
+++ UseCase1PalindromeCheckerApp.java
@@ -1,22 +1,21 @@
-public class UseCase1PalindromeCheckerApp {
-
+public class UseCase3PalindromeCheckerApp {
     public static void main(String[] args) {
 
         // Hardcoded input string
         String input = "madam";
+        String reversed = "";
 
-        boolean isPalindrome = true;
-
-        // Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
+        // Compare original and reversed using equals()
+        boolean isPalindrome = input.equals(reversed);
+
         // Display the result
-        System.out.println("Input text: " + input);
+        System.out.println("Input text    : " + input);
+        System.out.println("Reversed text : " + reversed);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
